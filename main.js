f16f74532f02c65b862ba226f9fed72aedd64a2a
Victoria_T <Tabordaingridvictoria@outlook.es>
2021-12-21 11:28:05 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,21 @@
+let pesoArg = document.querySelector('#pesoArg')
+let dolar = document.querySelector('#Dolar')
+let btn = document.querySelector('#btn')
+let resultado = document.querySelector('#resultado')
+
+
+btn.addEventListener('click',()=>{
+    let dolar_1 = 98
+    let pesoArg = document.querySelector('#pesoArg').value*dolar_1
+    resultado.innerHTML =  pesoArg
+
+})
+
+
+
+
+
+
+
+
+//ecuación se da dividiendo el valor del peso sobre el dólar
